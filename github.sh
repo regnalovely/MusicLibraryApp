b5e71cef344bb62bf54451d80a07ed22fdbcb4c6
regnalovely <regna.lovely@gmail.com>
2019-01-25 12:09:27 -0400

    Commande vocal + Lecture mp3 (Part 1)

--- github.sh
+++ github.sh
@@ -1,8 +1,9 @@
-git init
-git config --global user.email "regna.lovely@gmail.com"
-git config --global user.name "regnalovely"
+#git init
+#git config --global user.email "regna.lovely@gmail.com"
+#git config --global user.name "regnalovely"
 #git clone https://regnalovely@github.com/regnalovely/MusicLibraryApp.git
-git remote add android https://regnalovely@github.com/regnalovely/MusicLibraryApp.git
+git remote add devoir https://regnalovely@github.com/lpiem/projet-03.git
+#git remote add android https://regnalovely@github.com/regnalovely/MusicLibraryApp.git
 #git pull android master
 git add *
 git commit -m "Commande vocal + Lecture mp3 (Part 1) "
